5349d6d548a5560d833ed429282d956605e37c06
Lufter <lufter21@gmail.com>
2019-01-21 16:37:48 +0200

    com

--- dist/classes/SitemapXML.php
+++ dist/classes/SitemapXML.php
@@ -11,7 +11,7 @@ class SitemapXML extends Core {
 		$cont -> execute();
 		return $cont -> fetchAll(PDO::FETCH_OBJ);
 	}
-	
+
 	public function createSitemap() {
 		$this -> _xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?>'."\n".'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" />');
 		
